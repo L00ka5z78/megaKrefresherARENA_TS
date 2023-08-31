e418f7a902f3ca56e7549b94ffdd16e32636d349
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-08-31 15:22:40 +0200

    warrior record issue! 31.08.23 15:22

---
+++ records/warrior.record.ts
@@ -0,0 +1,9 @@
+export class WarriorRecord {
+  public id?: string;
+  public readonly name: string;
+  public readonly power: string;
+  public readonly defence: string;
+  public readonly stamina: string;
+  public readonly agility: string;
+  public readonly wins: string;
+}

1174c296baac17a0a08c8b0ab7685096c074b6fc
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-08-31 13:43:47 +0200

    main express configuration added 31.08.23 13:43

--- index.ts
+++ index.ts
@@ -1,1 +1,29 @@
-console.log('hello world');
+import express, { static as eStatic, urlencoded } from 'express';
+import 'express-async-errors';
+import methodOverride from 'method-override';
+import { engine } from 'express-handlebars';
+
+const app = express();
+
+app.use(methodOverride('_method'));
+app.use(
+  urlencoded({
+    extended: true,
+  })
+);
+app.use(eStatic('public'));
+app.engine(
+  'hbs',
+  engine({
+    extname: '.hbs',
+    //helpers: ???
+  })
+);
+app.set('view engine', '.hbs');
+app.get('/', (req, res) => {
+  res.send('HIIII!');
+});
+// app.use(handleError)
+app.listen(3000, 'localhost', () => {
+  console.log('Server is ON and running on http://localhost:3000');
+});

affdd08a25f22a760eba3433f2669a8bcc50351d
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-08-31 15:54:58 +0200

    warrior record constructor built 31.08.23 15:54

--- records/warrior.record.ts
+++ records/warrior.record.ts
@@ -1,9 +1,32 @@
 export class WarriorRecord {
   public id?: string;
+  /**
+   * namie is always unique. Example
+   * `console.log('abcdefg')  -writing own tips in help menu
+   * ```JS
+   *const obj = new warriorRecord();
+   * console.log(obj.name);
+   *
+   * const w = new WarriorRecord(); moved from index.ts
+   * w.
+   * ```
+   */
   public readonly name: string;
   public readonly power: string;
   public readonly defence: string;
   public readonly stamina: string;
   public readonly agility: string;
   public readonly wins: string;
+
+  constructor(obj: Omit<WarriorRecord, 'insert' | 'update'>) {
+    const { id, name, stamina, defence, power, wins, agility } = obj;
+
+    this.id = id;
+    this.wins = wins ?? 0;
+    this.name = name;
+    this.stamina = stamina;
+    this.power = power;
+    this.defence = defence;
+    this.agility = agility;
+  }
 }
